2ec4644a6661710f55e28cfcc71926a96f2efe24
emxile <80122657+emxile@users.noreply.github.com>
2025-12-25 12:32:42 +0100

    moved from other file

--- Ambient conditions.py
+++ Ambient conditions.py
@@ -1,7 +1,8 @@
 #Ambient conditions and Reference speed
 
 # Imports
-# import measuredData as data
+import measuredData as mData
+import math
 
 # Constants
 cMassAir = 29 * 10 ** (-3)  # kg / mol
@@ -11,11 +12,15 @@ cTemp = 273.15 # K
 cSutherland = 110.4 # K
 cChord = 0.2 # m
 
-# Measurements ! From wing average
-mTemp = 23.27 + cTemp # K
-mPres = 999.74705 * 100 # Pa 
-mPresDiff = 123.29 # Pa total pres in settling chamber - static pressure in contraction
-mPresTotal = mPres
+# Measurements ! From airfoil average
+#mTemp = (mData.airfoilData["T"].mean() + mData.wingData["T"].mean())/2 + cTemp # K
+mTemp = (mData.airfoilData["T"].mean() ) + cTemp # K
+mPres = (mData.airfoilData["P_bar"].mean() ) * 100 # Pa 
+#mPresDiff = (mData.airfoilData["Delta_Pb"].mean() + mData.wingData["Delta_Pb"].mean())/2 # Pa total pres in settling chamber - static pressure in contraction
+mPresDiff = (mData.airfoilData["Delta_Pb"].mean()) # Pa total pres in settling chamber - static pressure in contraction
+mPresTotal = (mData.airfoilData["P097"]).mean()
+
+print(f'Temp {mTemp}, mPres {mPres}, mPresDiff {mPresDiff}, total pressure {mPresTotal}')
 
 # Calculations
 
